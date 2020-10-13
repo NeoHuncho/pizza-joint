db9fa776a7a0d265a9489a5e5c23bff604474220
NeoHuncho <william.guinaudie@gmail.com>
2020-10-14 01:40:41 +0200

    added more animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,8 @@ const Home = () => {
   return (
     <motion.div className="home container"
     initial={{opacity:0}}
-    animate= {{opacity: 1}}>
+    animate= {{opacity: 1}}
+    transition={{delay:1.5, duration: 5}}>
       <h2>
         Welcome to Pizza Joint
       </h2>
